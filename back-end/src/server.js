d699cf15dea371b7baf9ea59d9162dc6a4b2755e
Arno Pan <apan11@my.bcit.ca>
2025-01-31 00:23:59 -0800

    mongodb completed corrected issues

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import {MongoClient, ServerApiVersion} from 'mongodb';
 
+// non-database JSON data
+const articleInfo = [
+    { name: 'learn-node', upvotes: 0, comments: [] },
+    { name: 'learn-react' ,upvotes: 0, comments: [] },
+    { name: 'mongodb', upvotes: 0, comments: [] },
+]
+
 const app = express();
 
 let db;
@@ -32,30 +39,38 @@ app.get("/api/articles/:name", async (req, res) => {
 
 app.post('/api/articles/:name/upvote', async (req, res) => {
     const { name } = req.params;
-    const { postedBy, text} = req.body;
 
-    const updatedArticle = await db.collection('articles').findOneAndUpdate({ name }, {
-        $push: { comments: newComment}
-    }, {
-        returnDocument: "after",
-    })
-
-    res.json(updatedArticle);
+    const articles = await db.collection("articles").findOneAndUpdate({ name },
+        {
+            $inc: {
+                upvotes: 1
+            }
+        },
+        {
+            returnDocument: "after"
+        }
+    );
+    res.json(articles);
 })
 
 // comments endpoint
-app.post('/api/articles/:name/comments', (req, res) =>{
+app.post('/api/articles/:name/comments', async (req, res) =>{
     const {name} = req.params;
     // const name = req.params; will assign the entire req.params to name, you'll need to do req.params.name
     const { postedBy, text} = req.body;
     // this is the JSON information that is required in the body of the POST request
-    const article = articleInfo.find(a => a.name === name);
-    // we're finding a name that matches the article name in the articleInfo array
 
-    article.comments.push({
-        postedBy,
-        text
-    });
+    const newComment = { postedBy, text};
+
+    const article = await db.collection('articles').findOneAndUpdate({ name }, { 
+        $push: {
+            comments: { postedBy, text }
+        }
+     },
+     {
+        returnDocument: "after"
+     }
+    )
 
     res.json(article);
 });
